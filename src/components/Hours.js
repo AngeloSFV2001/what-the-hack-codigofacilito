960de2cfe76267e98fe9c3149e67b6426a1aaa1a
Angelo Fabara <86901733+AngeloSFV2001@users.noreply.github.com>
2024-08-28 19:04:52 +0000

    reto3

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,19 +1,2 @@
 // Placeholder to host a component
 
-import React from 'react';
-
-const Hours = () => {
-  const now = new Date();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
-
-  return (
-    <div>
-      <h2>Hora actual</h2>
-      <p>{`${hours}:${minutes}:${seconds}`}</p>
-    </div>
-  );
-};
-
-export default Hours;
